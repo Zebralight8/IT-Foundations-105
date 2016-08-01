50bfa340450c5d09309b389cf3ade9a9669667e6
Zebralight8 <zebralight@hotmail.com>
2016-07-31 19:13:33 -0700

    Please check homework 6

--- ClassAssignment070516/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ClassAssignment070516/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,33 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int x = 0;
-            int y = 1;
-
-            // Equals checks if the two objects are equal
-            if (x.Equals(y))
-            {
-                Console.WriteLine("x and y are equal");
-            }
-
-            // GetType is used to return what type
-            // the object is.
-            Type type = x.GetType();
-            Console.WriteLine("The type's name is {0}", type.FullName);
-
-            // ToString converts the type into a string
-            Console.WriteLine("x is {0}", x.ToString());
-
-            // But since all objects support ToString
-            // the following works as well. WriteLine
-            // simply calls ToString for you.
-            Console.WriteLine("x is {0}", x);
+    class BTestAverage {
 
 
-        }
-    }
+        public int iAvg;
+        public double dAvg;
+        public BTestAverage (int iAvg, double dAvg)
+        { public void Increment }
+
+    static void Main()
+    { BTestAverage b = new BTestAverage ()
+
+        int iAvg = }
 }
---
+++ Homework6/Assignment 6 Properties/Assignment 6 Properties/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6_Properties
+{
+    class Television
+    {
+        private bool isOn;
+        private int channel;
+        private int volume;
+
+        public bool IsOn
+        {
+            get
+            { return isOn; }
+        }
+
+        public bool TurnOn
+        {
+            get
+            { return isOn; }
+            set
+            { isOn = true; }
+        }
+
+        public bool TurnOff
+        {
+            get
+            { return isOn; }
+            set
+            { isOn = false; }
+        }
+
+        public int CurrentVolume
+        {
+            get
+            { return volume; }
+        }
+
+        public int IncreaseVolume
+        {
+            get { if (volume < 100); 
+             return volume; }
+            set
+            {
+                volume = volume + 1;
+            }
+        }
+
+        public int DecreaseVolume
+        {
+            get
+            { if (volume < 100);  return volume; }
+            set { volume = volume - 1; }
+        }
+
+        public int CurrentChannel
+        {
+            get
+            { return channel; }
+        }
+
+        public int IncreaseChannel
+        {get { if (channel > 0 && channel < 50) ;  return channel; }
+            set { channel = channel + 1; }
+        }
+
+        public int DecreaseChannel
+        {
+            get { if (channel > 0 && channel < 50) ; return channel; }
+            set { channel = channel - 1; }
+        }
+
+
+        class TestTV
+        {
+            static void Main()
+            {
+                Television tv = new Television();
+
+                if (!tv.IsOn)
+                {
+                    tv.isOn = true;
+                }
+
+                tv.channel = 3;
+
+                tv.volume++;
+                tv.volume++;
+                tv.volume++;
+                tv.volume++;
+
+                tv.isOn = false;
+            }
+        }
+    }
+}
+
+        
+    
